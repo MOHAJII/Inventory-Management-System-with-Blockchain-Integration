a5e9a4e9899bbb31e8025d563c89aa41d65fd69f
Pradeep Antil <hmohammediaji@gmail.com>
2025-01-03 22:36:52 +0100

    update history and inventory page

--- src/main/java/inventoryManagement/dao/TransactionDAO.java
+++ src/main/java/inventoryManagement/dao/TransactionDAO.java
@@ -5,5 +5,11 @@ import inventoryManagement.dao.entities.Supplier;
 import inventoryManagement.dao.entities.Transaction;
 import org.bson.types.ObjectId;
 
+import java.util.List;
+
 public interface TransactionDAO extends DAO<Transaction, ObjectId> {
+//    public List<Transaction> getPurchasesTransaction();
+//    public List<Transaction> getSaleTransaction();
+//    public List<Transaction> getReturnTransaction();
+//    public List<Transaction> getDamageTransaction();
 }

4767742495e1813ddacf527420486158982761b4
Pradeep Antil <hmohammediaji@gmail.com>
2025-01-04 09:27:36 +0100

    Separate pages

--- src/main/java/inventoryManagement/viewController/DashboardController.java
+++ src/main/java/inventoryManagement/viewController/DashboardController.java
@@ -1,4 +1,65 @@
 package inventoryManagement.viewController;
 
-public class DashboardController {
+import inventoryManagement.dao.entities.Product;
+import inventoryManagement.service.InventoryService;
+import inventoryManagement.service.ProductService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    private ObservableList<Product> productObservableList = FXCollections.observableArrayList();
+    private ProductService productService;
+    private InventoryService inventoryService;
+
+    @FXML
+    private Label turnOver, totalStock;
+
+    @FXML
+    private BarChart<String, Double> inventoryBarChat;
+
+    @FXML
+    private TableColumn<Product, String> nameColumn;
+
+    @FXML
+    private TableColumn<Product, Double> priceColumn;
+
+    @FXML
+    private TableView<Product> productView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        productService = new ProductService();
+        inventoryService = new InventoryService();
+
+        productObservableList.addAll(productService.getAll());
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        productView.setItems(productObservableList);
+        inventoryBarChartInitializer();
+    }
+
+    public void inventoryBarChartInitializer() {
+        XYChart.Series<String, Double> inventorySeries = new XYChart.Series<>();
+        inventorySeries.setName("Inventories");
+        Map<String, Double> inventoryProduct = inventoryService.getInventoryProduct();
+        System.out.println(inventoryProduct.values());
+        inventoryProduct.forEach((pName, val) -> {
+            inventorySeries.getData().add(new XYChart.Data<>(pName, val));
+        });
+        inventoryBarChat.getData().add(inventorySeries);
+    }
 }
--- src/main/java/inventoryManagement/viewController/HomePageController.java
+++ src/main/java/inventoryManagement/viewController/HomePageController.java
@@ -16,6 +16,7 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,39 +24,19 @@ import java.net.URL;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-public class HomePageController implements Initializable {
+public class HomePageController implements Initializable{
 
-    private ObservableList<Product> productObservableList = FXCollections.observableArrayList();
-    private ProductService productService;
-    private InventoryService inventoryService;
 
     @FXML
     private ButtonBar dashboardBtn, historyBtn, settingBtn;
 
     @FXML
-    private Label turnOver, totalStock;
-
-    @FXML
-    private BarChart<String, Double> inventoryBarChat;
-
-    @FXML
-    private TableColumn<Product, String> nameColumn;
-
-    @FXML
-    private TableColumn<Product, Double> priceColumn;
-
-    @FXML
-    private TableView<Product> productView;
-
-    @FXML
-    private AnchorPane contentArea;
+    private StackPane contentArea;
 
     public void openDashboardPage(Event event) throws IOException {
-        Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/home-page.fxml"));
-        Scene newScene = new Scene(fxmlLoader.load(), 900, 500);
-        stage.setScene(newScene);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/dashboard-page.fxml"));
+        AnchorPane anchorPane = fxmlLoader.load();
+        contentArea.getChildren().setAll(anchorPane);
     }
 
     public void openProductPage(Event event) throws IOException {
@@ -85,27 +66,10 @@ public class HomePageController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        productService = new ProductService();
-        inventoryService = new InventoryService();
-
-        productObservableList.addAll(productService.getAll());
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-
-        productView.setItems(productObservableList);
-        inventoryBarChartInitializer();
+        try {
+            openDashboardPage(null);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-    public void inventoryBarChartInitializer() {
-        XYChart.Series<String, Double> inventorySeries = new XYChart.Series<>();
-        inventorySeries.setName("Inventories");
-        Map<String, Double> inventoryProduct = inventoryService.getInventoryProduct();
-        System.out.println(inventoryProduct.values());
-        inventoryProduct.forEach((pName, val) -> {
-            inventorySeries.getData().add(new XYChart.Data<>(pName, val));
-        });
-        inventoryBarChat.getData().add(inventorySeries);
-    }
-
-
 }
